a1d669591e0a14bf6a5f3fde472e01b4e45cbcad
LisPI <paschka.lis@gmail.com>
2019-09-04 17:52:56 +0300

    app limit opened pages

--- window.js
+++ window.js
@@ -1,35 +1,8 @@
-$(() => {
-})
+const puppeteer = require('puppeteer');
+const pLimit = require('p-limit');
+const limit = pLimit(2);
 
-function makeScreenshotByUrl(indexNumber, url, screenshotFilename) {
-  const puppeteer = require('puppeteer');
-
-  (async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.setViewport({
-      width: 1280,
-      height: 720,
-      deviceScaleFactor: 1,
-    });
-    try {
-      await Promise.race([
-        page.goto(url, {waitUntil: 'networkidle2'}),
-        new Promise(x => require('electron').remote.getGlobal('setTimeout')(x, 30000).unref()),
-      ]);
-    }
-    catch (e) {
-      alert('Error ' + e + ' URL:' + url)
-    }
-    await page.screenshot({path: require('path').join(document.getElementById('path-output').innerText, indexNumber + '. ' +screenshotFilename+'.png')});
-    await browser.close();
-
-  })();
-}
-
-function getNameForPngByUrl(url) {
-  return url.replace(/\//gi,'').replace(/\:/gi,'').replace(/\?/gi,'');
-}
+var pathForSavedScreenshot = ""
 
 function go() {
   if(document.getElementById('path-output').innerText == ''){
@@ -40,11 +13,55 @@ function go() {
     alert("Add urls please.")
     return
   }
+  pathForSavedScreenshot = document.getElementById('path-output').innerText
+  runBrowser();
+}
+
+async function runBrowser() {
+  // var t = { p : 0};
+  // Object.observe('t', function (id, oldval, newval) {
+  //   document.getElementById('active-count').innerText = newval.toString();
+  //   return newval;
+  // });
+
+
+  const browser = await puppeteer.launch({headless: true});
+
   var arrayOfUrls = document.getElementById('urls-input').value.split('\n');
+  var promises = []
   for(var i = 0; i < arrayOfUrls.length; i++){
     var name = getNameForPngByUrl(arrayOfUrls[i]);
-    makeScreenshotByUrl(i+1, arrayOfUrls[i], name);
+    promises.push(limit(makeScreenshotByUrl,browser, i + 1, arrayOfUrls[i], name));
   }
+
+
+
+  await Promise.all(promises)
+  await browser.close();
+}
+
+async function makeScreenshotByUrl(browser, indexNumber, url, screenshotFilename) {
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1280,
+    height: 720,
+    deviceScaleFactor: 1,
+  });
+  try {
+    await Promise.race([
+      page.goto(url, {waitUntil: 'networkidle2'}),
+      new Promise(x => require('electron').remote.getGlobal('setTimeout')(x, 30000).unref()),
+    ]);
+  }
+  catch (e) {
+    alert('Error ' + e + ' URL:' + url)
+  }
+  await page.screenshot({path: require('path').join(pathForSavedScreenshot, indexNumber + '. ' +screenshotFilename+'.png')});
+  await page.close();
+}
+
+function getNameForPngByUrl(url) {
+  return url.replace(/\//gi,'').replace(/\:/gi,'').replace(/\?/gi,'');
 }
 
 function selectOutputFolder() {
